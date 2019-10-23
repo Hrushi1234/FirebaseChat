abdc04e75faad7290d42aa3534efe86109e0de94
hrushi1234 <kuklarehn@gmail.com>
2019-10-23 22:15:07 +0530

    inital Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//var firebase = require("firebase/app");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDLj1Or5-hZu4FpxY9J2bMMB92np9u0qsE",
+  authDomain: "crud-4f54c.firebaseapp.com",
+  databaseURL: "https://crud-4f54c.firebaseio.com",
+  projectId: "crud-4f54c",
+  storageBucket: "crud-4f54c.appspot.com",
+  messagingSenderId: "417728642280",
+  appId: "1:417728642280:web:240026edeb12116e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//var database = firebase.database().ref('name');
+
+console.log(firebase);
